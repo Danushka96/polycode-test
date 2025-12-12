f3f3b1ae78c8f4856b9b9c0c6e7d665909b4f19d
danushka96 <danushkaherath96@gmail.com>
2025-12-12 12:29:15 +0530

    removed cors config

--- main.go
+++ main.go
@@ -5,17 +5,12 @@ import (
 	"pos-app/controllers"
 
 	runtime "github.com/cloudimpl/polycode-runtime/go"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "pos-app/.polycode"
 )
 
 func main() {
 	r := gin.Default()
-
-	config := cors.DefaultConfig()
-	config.AllowAllOrigins = true
-	r.Use(cors.New(config))
 
 	r.GET("/hello", controllers.HelloWithoutCache)
 	r.GET("/hello-cache", controllers.HelloWithCache)
