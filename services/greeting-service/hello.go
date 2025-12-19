c1cd9aae99507bd9945794c979046304c460b34e
danushka96 <danushkaherath96@gmail.com>
2025-12-19 15:27:03 +0530

    chore: added log message

--- services/greeting-service/hello.go
+++ services/greeting-service/hello.go
@@ -2,11 +2,13 @@ package greeting_service
 
 import (
 	"fmt"
-	"github.com/cloudimpl/polycode-runtime/go/sdk"
 	"pos-app/models"
+
+	"github.com/cloudimpl/polycode-runtime/go/sdk"
 )
 
 func Hello(ctx sdk.ServiceContext, req models.HelloRequest) (models.HelloResponse, error) {
+	ctx.Logger().Info().Msg("Hello request test log message")
 	return models.HelloResponse{
 		Message: fmt.Sprintf("Hello %s", req.Name),
 	}, nil
